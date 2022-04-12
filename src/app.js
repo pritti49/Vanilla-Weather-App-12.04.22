ee5ee60ed9b03431fdc13a8be0b3b140b939e5a0
Pritti Majithia <77355661+pritti49@users.noreply.github.com>
2022-04-12 19:53:27 +0100

    updated time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,25 @@
+function forematDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+let days = 
+["Sunday"
+ ,"Monday"
+  , "Tuesday"
+   , "Wednesday"
+   , "Thursday"
+   , "Friday"
+   , "Saturday"];
+let day = days[date.getDay()];
+return `${day} ${hours} ${minutes}`;
+}
+
 function displayTempreature(response) {
    
     let temperatureElement = document.querySelector("#temperature");
@@ -5,11 +27,13 @@ function displayTempreature(response) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElemenet = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = Math.round(response.data.main.humidity);
     windElemenet.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = forematDate(response.data.dt *1000);
 }
 
 let apiKey = "c68923fa1456eb2f9f1cb030550fbfa0";
