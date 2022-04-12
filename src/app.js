b62c12576a3d999db1238f7d3ed3a5acc5675554
Pritti Majithia <77355661+pritti49@users.noreply.github.com>
2022-04-12 20:24:20 +0100

    added icon ids and setAttributes

--- src/app.js
+++ src/app.js
@@ -28,12 +28,15 @@ function displayTempreature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElemenet = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = Math.round(response.data.main.humidity);
     windElemenet.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = forematDate(response.data.dt *1000);
+    iconElement.setAttribute ("src" ,`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute ("alt", response.data.weather[0]. description);
 }
 
 let apiKey = "c68923fa1456eb2f9f1cb030550fbfa0";
